b821464a71ddfbf77ab0a0e14d2b34405608fcad
Sokol Ikonomi <sokol.ikonomi@linksmt.it>
2021-01-17 21:49:43 +0100

    added menu calendar users rooms components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    RoomsComponent,
+    UsersComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
